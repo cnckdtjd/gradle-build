84d25437fc87031cb43ab1dccc9d194b64601586
ChangSeongChu <jacob@puzzlesystems.co.kr>
2023-11-22 10:39:55 +0900

    first

---
+++ src/main/java/com/cnckdtjd/gradlebuildtest/GradleBuildTestApplication.java
@@ -0,0 +1,13 @@
+package com.cnckdtjd.gradlebuildtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleBuildTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleBuildTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cnckdtjd/gradlebuildtest/GradleBuildTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnckdtjd.gradlebuildtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleBuildTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
